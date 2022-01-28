8afccfc0e309328821a84e230211514576750212
RobsonMT <rbsndev3@gmail.com>
2022-01-28 12:00:37 -0300

    feat: sliderContainer

---
+++ src/components/SliderContainer/index.tsx
@@ -0,0 +1,27 @@
+import "react-slideshow-image/dist/styles.css";
+import { Slide } from "react-slideshow-image";
+import { Box } from "@chakra-ui/react";
+
+export const SliderContainer = () => {
+  const slideImages = [
+    "https://media.kitsu.io/anime/cover_images/8699/original.jpg",
+    "https://media.kitsu.io/anime/cover_images/8063/original.png",
+    "https://media.kitsu.io/anime/cover_images/43321/original.png",
+    "https://media.kitsu.io/anime/cover_images/8576/original.jpg",
+    "https://media.kitsu.io/anime/cover_images/10028/original.jpg",
+  ];
+
+  return (
+    <Slide easing="ease">
+      {slideImages.map((image, index) => (
+        <Box
+          key={index}
+          backgroundImage={`url(${image})`}
+          backgroundPosition="center"
+          backgroundSize="cover"
+          height="330px"
+        />
+      ))}
+    </Slide>
+  );
+};
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
-import { Box } from '@chakra-ui/react'
-import { Header } from '../../components/Header'
+import { Box } from "@chakra-ui/react";
+import { Header } from "../../components/Header";
 // import { DarkModeButton } from '../../components/DarkModeButton'
+import { SliderContainer } from "../../components/SliderContainer";
 
 export const Home = () => {
   return (
@@ -8,6 +9,7 @@ export const Home = () => {
       <Header />
       {/* <DarkModeButton /> */}
       {/* <Text>Testando o Dark Mode</Text> */}
+      <SliderContainer />
     </Box>
-  )
-}
+  );
+};
